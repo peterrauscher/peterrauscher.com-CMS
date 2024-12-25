625913ed98ed627db83cb89bd859e0e09de8b95f
Peter Rauscher <peterrauscher@protonmail.com>
2024-12-25 17:41:39 -0500

    Update notion-hugo.config.ts

--- notion-hugo.config.ts
+++ notion-hugo.config.ts
@@ -4,7 +4,7 @@ const userConfig: UserConfig = {
     base_url: "https://notion-hugo.pages.dev",
     mount: {
         manual: false,
-        page_url: 'https://pcloud.notion.site/Notion-DoIt-04bcc51cfe4c49938229c35e4f0a6fb6',
+        page_url: 'https://peterrauscher.notion.site/Notion-Hugo-167c6bf0775780bca550c3c81264821c?pvs=4',
         pages: [
             // {
             //     page_id: '<page_id>',
